87cb6614455bb309d07a9a49f20101f84d7ebc2c
konLiogka <ntinaliogka@gmail.com>
2024-05-02 20:53:53 +0300

    init commit

---
+++ dnsconf.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+
+sudo systemctl start systemd-resolved
+sudo systemctl enable systemd-resolved
+
+sudo bash -c 'echo "nameserver 8.8.8.8"> /etc/resolv.conf'
+
+sudo bash -c 'echo "dns=none"> /etc/NetworkManager/NetworkManager.conf'
+
---
+++ launcher.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+declare -A categories
+categories["Messaging"]="discord session-messenger-desktop"
+categories["Browsing"]="floorp firefox"
+categories["Graphics"]="krita inkscape libreoffice"
+categories["Music"]="parole ToneLib-Zoom soulseekqt reaper audacity"
+categories["Coding"]="code arduino qtcreator"
+categories["Tools"]="lxappearance localsend virt-manager fdm"
+categories["Networking"]="gns3 packettracer wireshark"
+categories["Games"]="steam supertuxkart omori tr1x terraria undertale rainworld hollowknight plagueinc stardew"
+
+state="categories"
+while true; do
+    if [ "$state" = "categories" ]; then
+        selected_category=$(printf '%s\n' "${!categories[@]}" | rofi -dmenu -p "Categories")
+        
+        if [ -n "$selected_category" ]; then
+            apps=${categories[$selected_category]}
+            chosen=$(echo "$apps" | tr ' ' '\n' | rofi -dmenu -p "$selected_category")
+            if [ -n "$chosen" ]; then
+                exec bash -i -c "$chosen" 
+            fi 
+        else
+            break
+        fi
+    elif [ "$state" = "applications" ]; then
+        state="categories"
+    fi
+done
