055e68a4de9d3b441b74f6cafac8d762645bdeb1
Konstantina Liogka <78957746+konLiogka@users.noreply.github.com>
2024-05-03 22:28:33 +0300

    Update dnsconf.sh

--- dnsconf.sh
+++ dnsconf.sh
@@ -4,11 +4,8 @@
 sudo systemctl start systemd-resolved
 sudo systemctl enable systemd-resolved
 
-sudo bash -c 'echo "nameserver 8.8.8.8"> /etc/resolv.conf'
 
-<<<<<<< HEAD
 sudo bash -c 'echo -e "[main]\ndns=none" > /etc/NetworkManager/NetworkManager.conf'
-=======
-sudo bash -c 'echo -e "[main]/dns=none"> /etc/NetworkManager/NetworkManager.conf'
->>>>>>> 37adf42d049f1897ed6d695a38e1166ce09efc6a
+
+
 
