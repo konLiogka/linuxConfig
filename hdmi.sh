b16541efba10b0f3c65c74aefa41bfc394909c6e
konLiogka <ntinaliogka@gmail.com>
2024-05-03 22:23:13 +0300

    Update hdmi

--- hdmi.sh
+++ hdmi.sh
@@ -2,5 +2,16 @@
 
 #xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
 #xrandr --addmode  HDMI1 "1920x1080_60.00"
-xrandr --output HDMI1 --mode "1920x1080" --right-of eDP1
 
+if [ $# -eq 0 ]; then
+    echo "UwU"
+    exit 1
+fi
+if [ $1 -eq 1 ]; then
+    xrandr --output HDMI1 --mode "1920x1080" --right-of eDP1
+elif [ $1 -eq 0 ]; then
+    xrandr --output HDMI1 --off
+else
+    echo "uwu?"
+    exit 1
+fi
